fa68ff3cf78926d2f8b7a180edb5b983de4a50db
Guo Xiaoyi <guoxiaoyi888@gmail.com>
2015-05-20 16:53:06 +0800

    首页布局

---
+++ app/assets/javascripts/IEhack.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  jQuery("*:first-child").addClass("first-child"),jQuery("*:last-child").addClass("last-child");
+});
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :articles, only: [:index, :show],path: 'list'
   end
   resources :articles, only: [:index, :show]
-  resources :pages, only: [:show]
+  resources :pages, only: [:show], path: 'p'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

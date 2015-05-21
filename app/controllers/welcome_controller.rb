e058213d61d3d1df11b36886250e3e6e9d140561
Guo Xiaoyi <guoxiaoyi888@gmail.com>
2015-05-21 18:28:56 +0800

    首页列表

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    news_hash = {title_length: 22, style: 'panel-info', head_prefix: '', with_time: true}
+    @bbs = PannelList.new(:bbs, news_hash)
+    @study = PannelList.new(:study, news_hash)
+    @news = PannelList.new(:news, news_hash)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def pannel_list( pannel_obj )
+    render partial: 'layouts/pannel_list', locals:{ pannel_list: pannel_obj}
+  end
+
+  def truncate_u(text, length = 30, truncate_string = "...")
+    l=0
+    char_array=text.unpack("U*")
+    char_array.each_with_index do |c,i|
+      l = l+ (c<127 ? 0.5 : 1)
+      if l>=length
+        return char_array[0..i].pack("U*")+(i<char_array.length-1 ? truncate_string : "")
+      end
+    end
+    return text
+  end
 end

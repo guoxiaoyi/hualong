5816deb73054a1922b3c076d858cfbd9ceba856d
Guo Xiaoyi <guoxiaoyi888@gmail.com>
2015-05-21 12:02:22 +0800

    整体布局

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Time::DATE_FORMATS[:default] = "%Y/%m/%d %H:%M"
+Time::DATE_FORMATS[:date] = "%Y/%m/%d"
+Date::DATE_FORMATS[:month] = I18n.t('date.formats.month')
+Date::DATE_FORMATS[:short] = "%m月%d日"

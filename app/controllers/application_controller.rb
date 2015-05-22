66477dce5d60e39ec5f51a90272b9d4ca87a39a5
Guo Xiaoyi <guoxiaoyi888@gmail.com>
2015-05-22 15:25:02 +0800

    过滤html标签

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def right_page
-    @learning =WizcmsArticle::Page.get(:learning)
-    @enterprise =WizcmsArticle::Page.get(:enterprise)
+    @learning =WizcmsArticle::Page.get(:learning).content
+    @enterprise =WizcmsArticle::Page.get(:enterprise).content
     @see = PannelList.new(:see)
   end
 end

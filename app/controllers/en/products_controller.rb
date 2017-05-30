13e88b7af900286cb4abdf7937dd10850bf10aba
GuoxiaoYi <guoxiaoyi888@gmail.com>
2017-05-31 00:11:05 +0800

    产品布局修改

--- app/controllers/en/products_controller.rb
+++ app/controllers/en/products_controller.rb
@@ -2,6 +2,11 @@ class En::ProductsController < ApplicationController
   def index
     @product_categories = ProductCategory.all
     @content = Content.new
+    if params[:cid]
+      @products = Product.where(product_category_id: params[:cid])
+    else
+      @products= Product.all
+    end
   end
   def show
     @content = Content.new
